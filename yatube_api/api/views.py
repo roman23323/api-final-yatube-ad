502e8aaaa11d3b99e4b79c45434eb1f8a9dbe32d
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-16 02:23:42 +0300

    feat: implement viewsets

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,54 @@
-# TODO:  Напишите свой вариант
+from rest_framework import generics, permissions, filters, viewsets
+from posts.models import Follow, Post, Comment, Group
+from .serializers import FollowSerializer, PostSerializer, CommentSerializer, GroupSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+	def has_object_permission(self, request, view, obj):
+		if request.method in permissions.SAFE_METHODS:
+			return True
+		return obj.author == request.user
+
+
+class FollowListCreateView(generics.ListCreateAPIView):
+	permission_classes = (permissions.IsAuthenticated,)
+	serializer_class = FollowSerializer
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ('following__username',)
+
+	def get_queryset(self):
+		return Follow.objects.filter(user=self.request.user)
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
+	permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+
+	def perform_create(self, serializer):
+		serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+	serializer_class = CommentSerializer
+	permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+
+	def get_queryset(self):
+		return Comment.objects.filter(post__id=self.kwargs.get('post_id'))
+
+	def perform_create(self, serializer):
+		post_id = self.kwargs.get('post_id')
+		post = Post.objects.get(pk=post_id)
+		serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Group.objects.all()
+	serializer_class = GroupSerializer
+	permission_classes = (permissions.AllowAny,)
